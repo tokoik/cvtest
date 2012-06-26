3bb9009aed8d71f1304f7b06b280a0f6daf09d64
Kohe Tokoi <tokoik@gmail.com>
2012-06-26 23:45:09 +0900

    update dependency

--- cvtest.d
+++ cvtest.d
@@ -1,3 +1,21 @@
-gg.o: gg.cpp gg.h
-main.o: main.cpp gg.h matrix.h
-matrix.o: matrix.cpp matrix.h gg.h
+gg.o: gg.cpp gg.h /usr/X11R6/include/GL/glx.h \
+  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
+  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
+  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
+  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/gl.h \
+  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glxext.h \
+  /usr/X11R6/include/GL/glut.h /usr/X11R6/include/GL/glu.h
+main.o: main.cpp gg.h /usr/X11R6/include/GL/glx.h \
+  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
+  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
+  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
+  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/gl.h \
+  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glxext.h \
+  /usr/X11R6/include/GL/glut.h /usr/X11R6/include/GL/glu.h matrix.h
+matrix.o: matrix.cpp matrix.h gg.h /usr/X11R6/include/GL/glx.h \
+  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
+  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
+  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
+  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/gl.h \
+  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glxext.h \
+  /usr/X11R6/include/GL/glut.h /usr/X11R6/include/GL/glu.h

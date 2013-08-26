007b1626f50b5cb394f1deb0a02d9d34ba52ba29
Kohe Tokoi <tokoik@gmail.com>
2013-08-26 15:26:54 +0900

    use VBO for sphere

--- main.cpp
+++ main.cpp
@@ -46,16 +46,16 @@ static GLint dmapLoc;
 // テクスチャサイズ
 static GLint sizeLoc;
 
-/*
- * テクスチャ作成
- */
+//
+// テクスチャ作成
+//
 static void getTexture(void)
 {
   if (cvGrabFrame(capture))
   {
     // キャプチャ映像から画像の切り出し
     IplImage *image = cvRetrieveFrame(capture);
-   
+
     // 切り出した画像の種類の判別
     GLenum format;
     if (image->nChannels == 3)
@@ -70,23 +70,23 @@ static void getTexture(void)
     for (int y = 0; y < image->height; ++y)
     {
       glTexSubImage2D(GL_TEXTURE_2D, 0, 0, y, image->width, 1, format,
-                      GL_UNSIGNED_BYTE, image->imageData + image->widthStep * y);
+        GL_UNSIGNED_BYTE, image->imageData + image->widthStep * y);
     }
   }
 }
 
-/*
- * プログラム終了時の処理
- */
+//
+// プログラム終了時の処理
+//
 static void releaseCapture(void)
 {
   // image の release
   cvReleaseCapture(&capture);
 }
 
-/*
- * OpenCV の初期化
- */
+//
+// OpenCV の初期化
+//
 static void cvInit(void)
 {
   // カメラの初期化
@@ -99,99 +99,79 @@ static void cvInit(void)
   cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, WIDTH);
   cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
   cvSetCaptureProperty(capture, CV_CAP_PROP_FPS, 30.0);
-  
+
   // プログラム終了時に capture を release する
   atexit(releaseCapture);
 }
 
-/*
- * 球の頂点属性
- */
-static GLfloat (*pv)[3] = 0;
-static GLfloat (*nv)[3] = 0;
-static GLfloat (*tv)[2] = 0;
-static GLuint (*face)[3] = 0;
-static GLsizei faces = 0;
-
-/*
- * 球のデータ用のメモリの解法
- */
-static void releaseMemory(void)
+//
+// 球のデータを作成する
+//
+static GLuint makeSphere(float radius, int slices, int stacks, GLfloat sScale, GLfloat tScale)
 {
-  delete[] pv;
-  delete[] nv;
-  delete[] tv;
-  delete[] face;
-}
+  // 頂点配列オブジェクトの設定
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
 
-/*
- * 球のデータを作成する
- */
-static void makeSphere(float radius, int slices, int stacks)
-{
-  // テクスチャ座標のスケール
-  static const GLfloat sScale = (GLfloat)WIDTH / (GLfloat)(TEXWIDTH);
-  static const GLfloat tScale = (GLfloat)HEIGHT / (GLfloat)(TEXHEIGHT);
-  
-  // メモリの解放
-  releaseMemory();
-  
-  // 頂点用メモリの確保
+  // 頂点バッファオブジェクトの設定
   int vertices = (slices + 1) * (stacks + 1);
-  pv = new GLfloat[vertices][3];
-  nv = new GLfloat[vertices][3];
-  tv = new GLfloat[vertices][2];
-  
-  // インデックス用メモリの確保
-  faces = slices * stacks * 2;
-  face = new GLuint[faces][3];
-  
+  int faces = slices * stacks * 2;
+  GLuint vbo[2];
+  glGenBuffers(2, vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof (GLfloat) * 8 * vertices, 0, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (GLuint) * 3 * faces, 0, GL_STATIC_DRAW);
+  GLfloat (*vertex)[8] = reinterpret_cast<GLfloat (*)[8]>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+  GLuint (*face)[3] = reinterpret_cast<GLuint (*)[3]>(glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY));
+
   // 頂点の位置とテクスチャ座標を求める
   for (int k = 0, j = 0; j <= stacks; ++j)
   {
-    float t = (float)j / (float)stacks;
-    float ph = 3.141593f * t;
-    float y = cosf(ph);
-    float r = sinf(ph);
-    
+    GLfloat t = static_cast<GLfloat>(j) / static_cast<GLfloat>(stacks);
+    GLfloat ph = 3.141593f * t;
+    GLfloat y = cos(ph);
+    GLfloat r = sin(ph);
+
     for (int i = 0; i <= slices; ++i)
     {
-      float s = (float)i / (float)slices;
-      float th = 2.0f * 3.141593f * s;
-      float x = r * cosf(th);
-      float z = r * sinf(th);
-      
+      GLfloat s = static_cast<GLfloat>(i) / static_cast<GLfloat>(slices);
+      GLfloat th = 2.0f * 3.141593f * s;
+      GLfloat x = r * cosf(th);
+      GLfloat z = r * sinf(th);
+
       // 頂点の座標値
-      pv[k][0] = x * radius;
-      pv[k][1] = y * radius;
-      pv[k][2] = z * radius;
-      
+      vertex[k][0] = x * radius;
+      vertex[k][1] = y * radius;
+      vertex[k][2] = z * radius;
+
       // 頂点の法線ベクトル
-      nv[k][0] = x;
-      nv[k][1] = y;
-      nv[k][2] = z;
-      
+      vertex[k][3] = x;
+      vertex[k][4] = y;
+      vertex[k][5] = z;
+
       // 頂点のテクスチャ座標値
-      tv[k][0] = s * sScale;
-      tv[k][1] = t * tScale;
-      
+      vertex[k][6] = s * sScale;
+      vertex[k][7] = t * tScale;
+
       ++k;
     }
   }
-  
+
   // 面の指標を求める
   for (int k = 0, j = 0; j < stacks; ++j)
   {
     for (int i = 0; i < slices; ++i)
     {
       int count = (slices + 1) * j + i;
-      
+
       /* 上半分 */
       face[k][0] = count;
       face[k][1] = count + 1;
       face[k][2] = count + slices + 2;
       ++k;
-      
+
       /* 下半分 */
       face[k][0] = count;
       face[k][1] = count + slices + 2;
@@ -199,41 +179,40 @@ static void makeSphere(float radius, int slices, int stacks)
       ++k;
     }
   }
-  
-  // 頂点配列オブジェクトの設定
-  glGenVertexArrays(1, &vaname);
-  glBindVertexArray(vaname);
+
+  glUnmapBuffer(GL_ARRAY_BUFFER);
+  glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+
   glEnableClientState(GL_VERTEX_ARRAY);
   glEnableClientState(GL_NORMAL_ARRAY);
   glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glVertexPointer(3, GL_FLOAT, 0, pv);
-  glNormalPointer(GL_FLOAT, 0, nv);
-  glTexCoordPointer(2, GL_FLOAT, 0, tv);
-  
-  // プログラム終了時にメモリを解放する
-  atexit(releaseMemory);
+  glVertexPointer(3, GL_FLOAT, sizeof (GLfloat) * 8, reinterpret_cast<GLfloat *>(0));
+  glNormalPointer(GL_FLOAT, sizeof (GLfloat) * 8, reinterpret_cast<GLfloat *>(0) + 3);
+  glTexCoordPointer(2, GL_FLOAT, sizeof (GLfloat) * 8, reinterpret_cast<GLfloat *>(0) + 6);
+
+  return vao;
 }
 
-/*
- * OpenGL の初期化
- */
+//
+// OpenGL の初期化
+//
 static void glInit(void)
 {
   // ゲームグラフィックス特論の都合にもとづく初期化
   ggInit();
-  
+
   // シェーダプログラムの読み込み
   progname = ggLoadShader("simple.vert", "simple.frag");
-  
+
   // テクスチャユニット用の uniform 変数
   dmapLoc = glGetUniformLocation(progname, "dmap");
-  
+
   // テクスチャサイズの uniform 変数
   sizeLoc = glGetUniformLocation(progname, "size");
-  
+
   // 図形データの作成
-  makeSphere(1.0f, SLICES, STACKS);
-  
+  vaname = makeSphere(1.0f, SLICES, STACKS, (GLfloat)WIDTH / (GLfloat)TEXWIDTH, (GLfloat)HEIGHT / (GLfloat)TEXHEIGHT);
+
   // テクスチャメモリの確保
   glGenTextures(1, &texname);
   glBindTexture(GL_TEXTURE_2D, texname);
@@ -242,20 +221,20 @@ static void glInit(void)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  
+
   // テクスチャはそのまま表示する（ポリゴン色を無視する）
   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);  
-  
+
   // 隠面消去
   glEnable(GL_DEPTH_TEST);
-  
+
   // 背景色
   glClearColor(1.0, 1.0, 1.0, 1.0);
 }
 
-/*
- * 画面表示
- */
+//
+// 画面表示
+//
 static void display(void)
 {
   // 時刻の計測
@@ -266,14 +245,14 @@ static void display(void)
 
   // テクスチャ作成
   getTexture();
-  
+
   // 画面クリア
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  
+
   // モデルビュー変換行列の設定
   glLoadIdentity();
   gluLookAt(0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-  
+
   // 図形描画
   glBindTexture(GL_TEXTURE_2D, texname);
   glBindVertexArray(vaname);
@@ -282,62 +261,62 @@ static void display(void)
   glUniform2f(sizeLoc, TEXWIDTH, TEXHEIGHT);
   glPushMatrix();
   glRotated(t * 360.0, 0.0, 1.0, 0.0);
-  glDrawElements(GL_TRIANGLES, faces * 3, GL_UNSIGNED_INT, face);
+  glDrawElements(GL_TRIANGLES, SLICES * STACKS * 2 * 3, GL_UNSIGNED_INT, 0);
   glPopMatrix();
-  
+
   // ダブルバッファリング
   glutSwapBuffers();
 }
 
-/*
- * ウィンドウのリサイズ
- */
+//
+// ウィンドウのリサイズ
+//
 static void resize(int w, int h)
 {
   // ウィンドウ全体を表示領域にする
   glViewport(0, 0, w, h);
-  
+
   // 投影変換行列を設定する
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(40.0, (GLdouble)w / (GLdouble)h, 1.0, 5.0);
-  
+
   // モデルビュー変換行列に切り替える
   glMatrixMode(GL_MODELVIEW);
 }
 
-/*
- ** アニメーション
- */
+//
+// アニメーション
+//
 static void idle(void)
 {
   glutPostRedisplay();
 }
 
-/*
- * キー操作の処理
- */
+//
+// キー操作の処理
+//
 static void keyboard(unsigned char key, int x, int y)
 {
   switch (key)
   {
-    case 'q':
-    case 'Q':
-    case '\033':  // '\033' は ESC の ASCII コード
-      exit(0);
-    default:
-      break;
+  case 'q':
+  case 'Q':
+  case '\033':  // '\033' は ESC の ASCII コード
+    exit(0);
+  default:
+    break;
   }
 }
 
-/*
- * メインプログラム
- */
+//
+// メインプログラム
+//
 int main(int argc, char *argv[])
 {
   // OpenCV の初期化
   cvInit();
-  
+
   // GLUT の初期化
   glutInit(&argc, argv);
   glutInitWindowSize(WIDTH, HEIGHT);
@@ -347,12 +326,12 @@ int main(int argc, char *argv[])
   glutReshapeFunc(resize);
   glutIdleFunc(idle);
   glutKeyboardFunc(keyboard);
-  
+
   // OpenGL の初期化
   glInit();
-  
+
   // メインループ
   glutMainLoop();
-  
+
   return 0;
 }
